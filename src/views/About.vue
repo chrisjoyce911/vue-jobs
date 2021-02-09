7f9665c8d20b9d1ea7f0b055b966e6223107b12d
Chris Joyce <chris@Chriss-iMac.modem>
2021-02-09 14:26:45 +1000

    Router Links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam qui
+    placeat iste nihil, dolore explicabo adipisci deleniti nulla possimus quidem
+    minima vitae. Itaque praesentium nemo adipisci vel beatae repudiandae
+    officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+    eveniet ab tenetur laborum illo, aperiam, reprehenderit ipsam fugiat
+    officiis totam, provident veritatis ipsum consequuntur dolor? Labore
+    adipisci cum aut magnam.
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HomePage</h1>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat qui ad
+    necessitatibus quam modi magni et quos ab. Tenetur recusandae similique,
+    nihil aliquid possimus deleniti provident nostrum fugiat nulla culpa. Lorem
+    ipsum dolor sit amet consectetur adipisicing elit. Consectetur, earum porro
+    obcaecati accusantium esse voluptatem debitis et. Quibusdam delectus rem
+    excepturi dolorem dolores nulla omnis, ipsa doloribus vitae odit est. Lorem,
+    ipsum dolor sit amet consectetur adipisicing elit. Amet nihil beatae officia
+    ullam modi architecto, maiores sunt rem, eaque possimus illum ducimus
+    dignissimos eos explicabo nobis, maxime qui consectetur suscipit.
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
